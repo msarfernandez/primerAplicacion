d7c82cfd67581e644a797ef995cdb14e3cd589b7
Cristhian Duran <durancristhian@gmail.com>
2014-04-03 00:27:26 -0300

    Minificación del .css (un golazo!)

--- gruntfile.js
+++ gruntfile.js
@@ -55,6 +55,15 @@ module.exports = function(grunt) {
                 }
             }
         },
+        cssmin: {
+            minify: {
+                expand: true,
+                cwd: 'app/css/',
+                src: 'production.css',
+                dest: 'app/css/',
+                ext: '.min.css'
+            }
+        },
         encodeImages: {
             build: {
                 files: [{
@@ -81,10 +90,11 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks("grunt-encode-images");
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', []);
     grunt.registerTask('build', ['jshint', 'concat', 'stylus', 'encodeImages']);
-    grunt.registerTask('release', ['jshint', 'bower', 'concat', 'uglify', 'stylus', 'encodeImages']);
+    grunt.registerTask('release', ['jshint', 'bower', 'concat', 'uglify', 'stylus', 'cssmin', 'encodeImages']);
 };
