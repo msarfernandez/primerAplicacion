bcc9aaa33cb46b5b1fdebfc88f28d23b8e28c69e
Cristhian Duran <durancristhian@gmail.com>
2014-04-05 14:02:36 -0300

    Mejoras en navbar, social-bar, botones y grunt

--- gruntfile.js
+++ gruntfile.js
@@ -56,22 +56,15 @@ module.exports = function(grunt) {
             }
         },
         cssmin: {
+            options: {
+                banner: '/* durancristhian@gmail.com */'
+            },
             minify: {
                 expand: true,
                 cwd: 'app/css/',
                 src: 'production.css',
                 dest: 'app/css/',
                 ext: '.min.css'
-            }
-        },
-        encodeImages: {
-            build: {
-                files: [{
-                    expand: true,
-                    cwd: 'app/css/',
-                    src: 'production.min.css',
-                    dest: 'app/css/'
-                }]
             }
         },
         watch: {
@@ -91,10 +84,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-stylus');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks("grunt-encode-images");
+    grunt.loadNpmTasks("grunt-image-embed");
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', []);
     grunt.registerTask('build', ['jshint', 'concat', 'stylus']);
-    grunt.registerTask('release', ['jshint', 'bower', 'concat', 'uglify', 'stylus', 'cssmin', 'encodeImages']);
+    grunt.registerTask('release', ['jshint', 'concat', 'uglify', 'stylus', 'cssmin']);
 };
