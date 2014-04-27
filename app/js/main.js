24146510b01423261da6ef17c0c2cdd31043b7b7
Cristhian Duran <durancristhian@gmail.com>
2014-04-27 00:41:51 -0300

    Se agrega el mega menú

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
 $(window).on("load", function(){
 
 	console.log("Funciona!");
+
+	$("a").on("click", function (event) {
+
+		event.preventDefault();
+	});
+
+	$("#show-menu").on("click", function () {
+		
+		// Anima el botón		
+		$(this).find("i").toggleClass("clicked");
+
+		// Muestra el menú
+		$("nav").toggleClass("show-menu");
+	});
 });
--- app/js/production.js
+++ app/js/production.js
@@ -9344,4 +9344,18 @@ d[0].offsetTop||15===d[0].offsetTop;d.remove();a.fixedPosition=e}f.extend(b.defa
 $(window).on("load", function(){
 
 	console.log("Funciona!");
+
+	$("a").on("click", function (event) {
+
+		event.preventDefault();
+	});
+
+	$("#show-menu").on("click", function () {
+		
+		// Anima el botón		
+		$(this).find("i").toggleClass("clicked");
+
+		// Muestra el menú
+		$("nav").toggleClass("show-menu");
+	});
 });
