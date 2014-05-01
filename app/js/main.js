e1d2f09d8b0bf721d48a25632b8997b9d4288e57
Cristhian Duran <durancristhian@gmail.com>
2014-05-01 16:00:27 -0300

    Se completa la portada y con funcionalidad. Falta animar todo

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,44 @@
 $(window).on("load", function(){
 
 	console.log("Funciona!");
+
+	// Muestra el video
+	$("#show-video").fancybox({
+		'padding'		: 0,
+		'autoScale'		: true,
+		'transitionIn'	: 'fade',
+		'transitionOut'	: 'fade',		
+		'type'			: 'swf',
+		'swf'			: {
+			'wmode'				: 'transparent',
+			'allowfullscreen'	: 'true'
+		}
+	});
+
+	// Muestra las imágenes del evento
+	$(".show-img").fancybox({
+		'padding'		: 0,
+		'autoScale'		: true,
+		'transitionIn'	: 'fade',
+		'transitionOut'	: 'fade'
+	});
+
+	// Muestra las descripciones de los músicos
+	$(".musician a.link").on("click", function (event) {
+
+		event.preventDefault();
+
+		if($(this).hasClass("clicked")) {
+
+			$(this).removeClass("clicked");
+			$(this).html("<span>Más</span><i class='fa fa-angle-double-right'></i>");
+		}
+		else {
+
+			$(this).addClass("clicked");
+			$(this).html("<span>Ocultar</span><i class='fa fa-angle-double-left'></i>");
+		}
+
+		$(this).siblings().slideToggle();
+	});
 });
